05cac0d956489f41adcb0bb9ca9546d66ffff852
HELAL-UDDIN-BISWAS <mdhelal6775@gmail.com>
2024-02-20 13:04:10 +0600

    Delete success

--- index.js
+++ index.js
@@ -35,7 +35,12 @@ async function run() {
     res.send(result)
   })
 
-  app.delete()
+  app.delete('/deleteproduct/:id',async(req,res)=>{
+    const id= req.params.id;
+    const query={_id : new ObjectId(id)};
+    const result =await Products.deleteOne(query);
+    res.send(result) 
+  })
 
 
     // Connect the client to the server	(optional starting in v4.7)
