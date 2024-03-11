e0485b28d6323cd95c5becc8bd7a0cab56c24882
HELAL-UDDIN-BISWAS <mdhelal6775@gmail.com>
2024-03-11 19:55:12 +0600

    add user purchase product

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const dotenv = require('dotenv').config();
 const cors = require("cors");
 var cookieParser = require('cookie-parser')
-const port = process.env.PORT || 5000; 
+const port = process.env.PORT || 5000;
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 app.use(express.json());
 app.use(cors('http://localhost:5173/'));
@@ -24,44 +24,52 @@ async function run() {
   try {
     const Products = client.db("tea-and-coffee").collection("allproducts");
     const ProductCart = client.db("tea-and-coffee").collection("productcart");
+    const userPurchaseProduct = client.db("tea-and-coffee").collection("purchaseproduct");
 
-  //  Get All Data
+    //  Get All Data
     app.get('/allproducts', async (req, res) => {
-      const result =await Products.find().toArray();
+      const result = await Products.find().toArray();
       res.send(result)
     })
 
-  //  Get ID Data
-  app.get('/product/:id', async(req,res)=>{
-    const id=req.params.id;
-    const query={_id : new ObjectId(id)}
-    const result= await Products.findOne(query);
-    res.send(result)
-  })
-
-  app.delete('/deleteproduct/:id',async(req,res)=>{
-    const id= req.params.id;
-    const query={_id : new ObjectId(id)};
-    const result =await Products.deleteOne(query);
-    res.send(result) 
-  })
- 
-  app.post('/addproduct',async(req,res)=>{
-    const document = req.body;
-      const result=await Products.insertOne(document)
+    //  Get ID Data
+    app.get('/product/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) }
+      const result = await Products.findOne(query);
       res.send(result)
-  })
+    })
 
-  // Add To Cart
-  app.post('/addcartproduct',async(req,res)=>{
-    const document=req.body;
-    const result= await ProductCart.insertOne(document);
-    res.send(result)
-  })
-app.get('/cartdata',async(req,res)=>{
- const result= await ProductCart.find.toArray();
- res.send(result)
-})
+    app.delete('/deleteproduct/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await Products.deleteOne(query);
+      res.send(result)
+    })
+
+    app.post('/addproduct', async (req, res) => {
+      const document = req.body;
+      const result = await Products.insertOne(document)
+      res.send(result)
+    })
+
+    // Add To Cart
+    app.post('/addcartproduct', async (req, res) => {
+      const document = req.body;
+      const result = await ProductCart.insertOne(document);
+      res.send(result)
+    })
+    app.get('/cartdata', async (req, res) => {
+      const result = await ProductCart.find().toArray();
+      res.send(result)
+    })
+    // Add To Cart
+// User Product
+ app.post("/adduserproduct",async(req,res)=>{
+  const document =req.body;
+  const result=await userPurchaseProduct.insertOne(document);
+  res.send(result)
+ })
     // Connect the client to the server	(optional starting in v4.7)
     // await client.connect();
     // Send a ping to confirm a successful connection
